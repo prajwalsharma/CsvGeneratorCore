16ff9ae48577ddbcd2bd50e7d882214702a6be56
prjwl4 <prjwl4@gmail.com>
2021-07-17 12:38:53 +0530

    Add project files.

---
+++ EmployeeRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvGeneratorCore
+{
+    public static class EmployeeRepository
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>() {
+                new Employee {
+                    ID = 1,
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Department = "Finance",
+                    Email = "john@smith.com",
+                    Location = "Seattle"
+                },
+                new Employee {
+                    ID = 2,
+                    FirstName = "Jane",
+                    LastName = "Smith",
+                    Department = "HR",
+                    Email = "john@smith.com",
+                    Location = "New York"
+                },
+                new Employee {
+                    ID = 3,
+                    FirstName = "Jenny",
+                    LastName = "Smith",
+                    Department = "Software Support Seervices",
+                    Email = "john@smith.com",
+                    Location = "California"
+                },
+                new Employee {
+                    ID = 4,
+                    FirstName = "Adam",
+                    LastName = "Smith",
+                    Department = "Networking",
+                    Email = "john@smith.com",
+                    Location = "Denver"
+                },
+                new Employee {
+                    ID = 5,
+                    FirstName = "Rambo",
+                    LastName = "Smith",
+                    Department = "Social Media",
+                    Email = "john@smith.com",
+                    Location = "Toronto"
+                },
+                new Employee {
+                    ID = 6,
+                    FirstName = "Jake",
+                    LastName = "Smith",
+                    Department = "Marketing",
+                    Email = "john@smith.com",
+                    Location = "London"
+                },
+                new Employee {
+                    ID = 7,
+                    FirstName = "Brandy",
+                    LastName = "Smith",
+                    Department = "HR",
+                    Email = "john@smith.com",
+                    Location = "Bangalore"
+                }
+            };
+
+            return employees;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+
+namespace CsvGeneratorCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Fetch data
+            var employees = EmployeeRepository.GetEmployees();
+
+            // 2. Set path to store csv files
+            string pathToSaveFiles = $"D:\\Programming\\.NET Standard\\CsvGeneratorCore\\CsvFiles\\employees.csv";
+
+            // 3. Generate CSV file
+            using (var writer = new StreamWriter(pathToSaveFiles))
+            {
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(employees);
+                }
+            }
+        }
+    }
+}
